32830c1795fcd1ddaed97ae5581d0c87d23730ab
vbelis <billbelis@yahoo.com>
2022-07-03 15:48:34 +0200

    add data folder to reproduce results, clean up  and rename notebook

---
+++ data/README.md
@@ -0,0 +1,2 @@
+# Folder containing the simulation results of the `batch_runner.py` script
+The dataframes contain the outcome of different simulation runs. You can check their sturcture and the insights that we gain from them in `src/plot_analysis.ipynb`
--- src/batch_runner.py
+++ src/batch_runner.py
@@ -1,12 +1,22 @@
-import matplotlib.pyplot as plt
+# Script for running simulations in parallel, scanning different parameter
+# values. The values that the batrch_runner will scan can be tweaked in the
+# dictionary bellow. The output is saved in a pandas dataframe pickle file.
+
+import argparse
 import numpy as np
-from random import sample
 from model import EmpireModel
 from mesa.batchrunner import batch_run
-from mesa.batchrunner import FixedBatchRunner
 import pandas as pd
 
-if __name__ == "__main__":
+
+def main():
+    """
+    Defines and runs model simulations in parallel, scanning the parameter space
+    defined by the `parameter_set` dictionary. Concatenates the outputs of the 
+    simulations in a single pandas dataframe and saves it into a .pkl file.
+    """
+
+    out_path = get_arguments()
     param_set = dict(
         height=50,  # Height and width are constant
         width=50,
@@ -18,10 +28,33 @@ if __name__ == "__main__":
         government_legitimacy=0.85,
         max_steps=100,
     )
-
     param_run = batch_run(
         EmpireModel, param_set, iterations=5, data_collection_period=-1
-    )  # , max_steps=1)
+    )
     print("Saving the dataframe to pickle file...")
     df = pd.DataFrame(param_run)
-    df.to_pickle("./df_50_3_jailtime_Lto0.pkl")
+    df.to_pickle(out_path)
+
+def get_arguments() -> str:
+    """
+    Parses command line arguments. In this case is simply one argument, the path
+    to the output file of the script.
+    
+    Returns: The output path of the dataframe produced in the simulation.
+    """
+    parser = argparse.ArgumentParser(formatter_class=
+                                 argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+    "--out",
+    type=str,
+    required=True,
+    help="Path to save the simulation results produced by the batchrunner in "
+          "dataframe format (.pkl file). Include the .pkl suffix.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+
+if __name__ == "__main__":
+    main()
